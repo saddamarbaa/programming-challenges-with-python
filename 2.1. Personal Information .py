de880352cc385700bf0f22796e9159b47297c358
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-10-04 17:28:33 +0700

    Program to displays Personal Information (chapter 2)

--- 2.1. Personal Information .py
+++ 2.1. Personal Information .py
@@ -1,1 +1,23 @@
+"""
+Starting out with Python, Third Edition, Tony Gaddis
+Chapter 2
+Programming Challenges
+1. Personal Information
+Write a program that displays the following information:
+Your name
+Your address, with city, state, and ZIP
+Your telephone number
+Your college major
+ Reference:
+(1) Starting out with Python, Third Edition, Tony Gaddis
+(2) https://youtu.be/O8UIQIyNgkQ """
 
+# Variable to store personal information
+name = "Saddam Arbaa"
+address = "Khroum Sudan 1238485858"
+telephoneNumber = 8548388853
+collgeMajor = 'IT'
+
+# printing the  personal information
+# sep = '\n' is mean separate the argument with new
+print(name, address, telephoneNumber, collgeMajor, sep = '\n')
