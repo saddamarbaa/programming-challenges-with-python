d5140c4af72e219edf7e07aa8ce90b21e3b4768a
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-10-07 15:00:55 +0700

    Distance Traveled - Python ,Tony Gaddis (Chapter 2)

--- 2.5. Distance Traveled.py
+++ 2.5. Distance Traveled.py
@@ -1,2 +1,20 @@
+"""
+5. Distance Traveled
 
- 
+Assuming there are no accidents or delays,
+the distance that a car travels down the inter-state can
+be calculated with the following formula: Distance = Speed * Time
+A car is traveling at 70 miles per hour. Write a program that displays the following:
+The distance the car will travel in 6 hours
+The distance the car will travel in 10 hours
+The distance the car will travel in 15 hours
+Reference:
+(1) Starting out with Python, Third Edition, Tony Gaddis
+(2) https://youtu.be/qD0ny262H3s
+"""
+Speed = 70
+# printing the final Result in Formated
+# Distance = Speed * Time
+print("The distance the car will travel in 6 hours is : " + str( (6 * Speed) ) + " miles")
+print("The distance the car will travel in 10 hours is : " + str( (10 * Speed) ) + " miles")
+print("The distance the car will travel in 15 hours is : " + str( (15 * Speed) ) + " miles")
